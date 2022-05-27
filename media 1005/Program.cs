0cc4565420769fff079acceebe0832e9edecbaa1
guest03 <guest03@CHEINFD14>
2022-05-26 21:42:27 -0300

    Adicionar arquivos de projeto.

---
+++ media 1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace media_1005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double N1 = double.Parse(Console.ReadLine());
+
+            double N2 = double.Parse(Console.ReadLine());
+
+            double med =N1 * 3.5;
+             double ia =N2 *7.5;
+            double media = med + ia;
+            double mediaTotal = media / 11;
+            Console.WriteLine($"MEDIA ={mediaTotal :.00000}");
+        }
+    }
+}
